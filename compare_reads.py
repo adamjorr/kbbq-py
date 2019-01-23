199c893c19c21b7eaa9722657180e41be48c4751
Adam Orr <adamjorr@gmail.com>
2019-01-23 07:55:04 +0000

    modify debug info to avoid bug

--- compare_reads.py
+++ compare_reads.py
@@ -658,9 +658,9 @@ def main():
         ne = (from_table != gatkcalibratedquals)
         print("ne.shape",ne.shape)
         print("sum(ne)", np.sum(ne))
-        print("from_table[ne][0,:]", from_table[np.any(ne, axis = 1),:][0,:])
-        print("gatkcalibratedquals[ne][0,:]", gatkcalibratedquals[np.any(ne, axis = 1),:][0,:])
-        print("ne[np.any(ne, axis = 1),:][0,:]", ne[np.any(ne, axis = 1),:][0,:])
+        print("from_table[ne][0,:]", from_table[np.any(ne, axis = 1),:])
+        print("gatkcalibratedquals[ne][0,:]", gatkcalibratedquals[np.any(ne, axis = 1),:])
+        print("ne[np.any(ne, axis = 1),:]", ne[np.any(ne, axis = 1),:])
         raise
 
     plot_calibration([rawquals.flatten(), gatkcalibratedquals.flatten(), dq_calibrated.flatten(), custom_gatk_calibrated.flatten(), from_table.flatten()],
