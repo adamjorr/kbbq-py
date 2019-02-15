e5a53f513d34c353d4f78102959f66f29c616442
Adam Orr <adamjorr@gmail.com>
2019-02-15 14:51:56 -0700

    work on documentation

--- recaltable.py
+++ recaltable.py
@@ -3,13 +3,27 @@ import numpy as np
 import pandas as pd
 
 class GATKReport:
+    """
+    A base class representing a GATK Report file that contains many tables.
+
+    Variables: 
+        header - a header line (such as #:GATKReport.v1.1:5)
+        tables - a list of GATKTable objects
+
+    Methods:
+        write - write the report to a file
+
+    """
+
     def __init__(self, filename):
+        """Initialize by reading a file specified by filename."""
         with open(filename) as fh:
             self.header = fh.readline()
             table_strings = fh.read().split('\n\n')
             self.tables = [GATKTable(s) for s in table_strings if s != '']
 
     def write(self, filename):
+        """Write the report to filename."""
         with open(filename, 'w') as fh:
             fh.write(self.header)
             for t in self.tables:
@@ -20,7 +34,34 @@ class GATKReport:
         pass #TODO
 
 class GATKTable:
+    """
+    A class representing a GATK report table.
+
+    The intended method of interacting with the table is using the data
+    attribute, which is a standard Pandas dataframe.
+
+    Variables:
+        format - the format string representing the data in the table.
+            This is like: #:GATKTable:ncol:nrow:%f:%f:%f:;
+            It is ':' delimited and ';' terminated.
+        ncols - The number of columns in the table
+        nrows - The number of rows in the table
+        fmtstrings - A list of the format strings for each column in the table
+        name - The entire line specifying the name of the table. It is
+            formatted like '#:GATKTable:title:subtitle'. subtitle can be
+            an empty string.
+        title - The title of the table
+        subtitle - The subtitle of the table
+        header - A list containing the title of each column
+        data - A Pandas dataframe containing the table data. Accessing this
+            attribute is the primary way to interact with the data.
+
+    Methods:
+        write - Write the table to a filehandle.
+    """
+
     def __init__(self, tablestring):
+        """Initialize the table from a string"""
         rows = tablestring.splitlines()
         self.format = rows[0]
         splitfmt = self.format.split(':')
@@ -28,7 +69,7 @@ class GATKTable:
         self.nrows = splitfmt[3]
         self.fmtstrings = splitfmt[4:-1]
         self.name = rows[1]
-        self.title = self.name.split(':')[1]
+        self.title = self.name.split(':')[2]
         self.header = rows[2].split()
         assert len(self.header) == len(self.fmtstrings)
         strdata = [s.split() for s in rows[3:]]
@@ -48,6 +89,7 @@ class GATKTable:
         self.data = self.data.astype(typedict)
 
     def write(self, filehandle):
+        """Write the table to a filehandle."""
         filehandle.writelines([self.format + '\n'] + [self.name + '\n'])
         datawidths = self.data.apply(lambda x: x.str.len().max()).get(self.header).values
         headwidths = np.array([len(s) for s in self.header])
