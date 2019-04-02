0f69245782c4ce70a2543e23e8160f275aa80657
Adam Orr <adamjorr@gmail.com>
2019-04-02 10:39:31 -0700

    add bam test as true test

--- tests/test_compare_reads.py
+++ tests/test_compare_reads.py
@@ -1,12 +1,16 @@
 import pytest
 from kbbq import compare_reads
+import numpy as np
 
 def test_bam_calibration(report, recalibratedbam):
     """
     Test that our python implementation of GATK calibration matches GATK
     """
-    rg_to_pu = compare_reads.get_id_to_pu(recalibratedbam)
+    rg_to_pu = compare_reads.get_rg_to_pu(recalibratedbam)
     rg_to_int = {r:i for i,r in enumerate(rg_to_pu)}
-    vectors = compare_reads.table_to_vectors(report, list(id_to_pu.values()))
-    dqs = get_delta_qs(*vectors)
-
+    meanq, vectors = compare_reads.table_to_vectors(report, list(rg_to_pu.values()))
+    dqs = get_delta_qs(meanq, *vectors)
+    for read in bam:
+        gatk_calibrated_quals = np.array(read.query_qualities, dtype = np.int)
+        recalibrated_quals = recalibrate_bamread(read, meanq, *dqs, rg_to_int, compare_reads.Dinucleotide.dinuc_to_int)
+        assert np.array_equal(recalibrated_quals, gatk_calibrated_quals)
