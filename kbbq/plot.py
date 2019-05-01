cece9642451a1644b425c101599861d09e2c8b34
Adam Orr <adamjorr@gmail.com>
2019-05-01 16:17:03 -0700

    clean plotting code a bit

--- kbbq/plot.py
+++ kbbq/plot.py
@@ -2,7 +2,10 @@
 import numpy as np
 import matplotlib
 matplotlib.use('Agg')
+import matlotlib.pyplot as plt
 import seaborn as sns
+import sklearn
+import kbbq.compare_reads as cr
 
 def plot_calibration(data, truth, labels, plotname, plottitle = None, maxscore = 42):
     print(tstamp(), "Making Quality Score Plot . . .", file = sys.stderr)
@@ -13,16 +16,15 @@ def plot_calibration(data, truth, labels, plotname, plottitle = None, maxscore =
     plt.title(plottitle)
     ax.plot(np.arange(maxscore + 1), 'k:', label = "Perfect")
     assert np.all([np.array_equal(data[0].shape, data[i].shape) for i in range(len(data))])
-    for i in range(len(data)):
-        label = labels[i]
+    for d, label in zip(data, labels):
         print(tstamp(), "Plotting %s . . ." % (label), file = sys.stderr)
-        assert np.ndim(data[i]) == 1
-        est_p = q_to_p(data[i])
+        assert np.ndim(d) == 1
+        est_p = cr.q_to_p(d)
         bscore = sklearn.metrics.brier_score_loss(truth.reshape(-1), est_p.reshape(-1))
-        numtotal = np.bincount(data[i].reshape(-1), minlength = (maxscore+1))
-        numerrs = np.bincount(data[i][truth].reshape(-1), minlength = len(numtotal)) #errors
+        numtotal = np.bincount(d.reshape(-1), minlength = (maxscore+1))
+        numerrs = np.bincount(d[truth].reshape(-1), minlength = len(numtotal)) #errors
         p = np.true_divide(numerrs[numtotal != 0],numtotal[numtotal != 0])
-        q = p_to_q(p)
+        q = cr.p_to_q(p)
         ax.plot(np.arange(len(numtotal))[numtotal != 0], q, 'o-', alpha = .8, label ="%s, %1.5f" % (label, bscore))
     plt.xlabel("Predicted Quality Score")
     plt.ylabel("Actual Quality Score")
@@ -35,11 +37,13 @@ def plot_samplesize(data, labels, plotname, plottitle = None, maxscore = 42):
     sns.set()
     ssplot, ax = plt.subplots(figsize = (9,9))
     ssplot.suptitle(plottitle)
-    for i in range(len(data)):
-        numtotal = np.bincount(data[i].reshape(-1), minlength = (maxscore+1))
-        ax.plot(np.arange(len(numtotal))[numtotal != 0], numtotal[numtotal != 0], 'o-', alpha = .6, label = f"{labels[i]}")
+    for d, label in zip(data, labels):
+        numtotal = np.bincount(d.reshape(-1), minlength = (maxscore+1))
+        ax.plot(np.arange(len(numtotal))[numtotal != 0], numtotal[numtotal != 0], 'o-', alpha = .6, label = f"{label}")
     plt.xlabel("Predicted Quality Score")
     plt.ylabel("Sample Size")
     ax.legend(loc = "upper left")
     ssplot.savefig(plotname)
 
+#def plot_calibration_mse():
+#
