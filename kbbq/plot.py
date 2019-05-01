fd362d9f22e25782e05b5df6ca9baa640cde280f
Adam Orr <adamjorr@gmail.com>
2019-05-01 16:25:43 -0700

    add plot docstrings

--- kbbq/plot.py
+++ kbbq/plot.py
@@ -8,6 +8,22 @@ import sklearn
 import kbbq.compare_reads as cr
 
 def plot_calibration(data, truth, labels, plotname, plottitle = None, maxscore = 42):
+    """
+    Plot true probability of each quality bin.
+
+    This plots the predicted quality score (x) against the true quality score (y)
+    for each quality bin. This is done by calculating the mean probability of error
+    for each bin and converting that probability back to a quality score.
+
+    :param list(np.array) data: list of 1d arrays of quality scores
+    :param np.array truth: boolean array where errors are True.
+    :param list(str) labels: list of labels to add to plot corresponding
+     to each data array
+    :param str plotname: name of file to save plot to
+    :param str plottitle: title of plot. If not provided, plotname is used instead.
+    :param int maxscore: max score to plot.
+
+    """
     print(tstamp(), "Making Quality Score Plot . . .", file = sys.stderr)
     assert np.ndim(truth) == 1
     plottitle = (plottitle if plottitle is not None else plotname)
@@ -32,6 +48,21 @@ def plot_calibration(data, truth, labels, plotname, plottitle = None, maxscore =
     qualplot.savefig(plotname, bbox_inches='tight')
 
 def plot_samplesize(data, labels, plotname, plottitle = None, maxscore = 42):
+    """
+    Plot the size of each quality bin.
+
+    This plots each quality bin (x) against the number
+    of bases assigned to that quality bin (y) as a 
+    line plot.
+
+    :param list(np.array) data: list of 1d arrays of quality scores
+    :param list(str) labels: list of labels to add to plot corresponding
+     to each data array
+    :param str plotname: name of file to save plot to
+    :param str plottitle: title of plot. If not provided, plotname is used instead.
+    :param int maxscore: max score to plot.
+
+    """
     print(tstamp(), "Making Sample Size Plot . . .", file = sys.stderr)
     plottitle = (plottitle if plottitle is not None else plotname)
     sns.set()
