1fc1e48e60a4f372b7b73de8a72a977caa3399f6
Adam Orr <adamjorr@gmail.com>
2019-05-07 14:40:22 -0700

    add benchmark to __all__

--- kbbq/__init__.py
+++ kbbq/__init__.py
@@ -10,4 +10,4 @@ from kbbq.compare_reads import *
 import kbbq.recaltable
 from kbbq.recaltable import *
 
-__all__ = ['compare_reads', 'recaltable', 'plot']
+__all__ = ['compare_reads', 'recaltable', 'plot', 'benchmark']
