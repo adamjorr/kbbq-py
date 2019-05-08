59e60e272f5c25c38a435cfbe0bd7cefd59eb974
Adam Orr <adamjorr@gmail.com>
2019-05-08 13:39:31 -0700

    stop init from polluting namespace

--- kbbq/__init__.py
+++ kbbq/__init__.py
@@ -6,9 +6,9 @@ A package for reference-free base quality score recalibration.
 """
 
 import kbbq.compare_reads
-from kbbq.compare_reads import *
 import kbbq.recaltable
-from kbbq.recaltable import *
+import kbbq.benchmark
+import kbbq.plot
 
 __all__ = ['compare_reads', 'recaltable', 'plot', 'benchmark']
 __version__ = '0.0.0'
