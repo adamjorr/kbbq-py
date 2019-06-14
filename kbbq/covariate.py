30b691eb06a90f6a22070e099babd02f320d0dfa
Adam Orr <adamjorr@gmail.com>
2019-06-14 16:52:32 -0700

    start a covariate class

--- kbbq/covariate.py
+++ kbbq/covariate.py
@@ -3,3 +3,13 @@ A data class and helper functions for read covariates.
 """
 
 #TODO
+
+class CovariateData():
+    """
+    A class for holding all the covariate data needed
+    to recalibrate reads. Class attributes will hold
+    the max values observed so far.
+    """
+    #nrgs will be available from the read class
+    seqlen = 0
+
