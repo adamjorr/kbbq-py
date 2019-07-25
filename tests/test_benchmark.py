ad38b1065de2a3b4acbc9b1c374f9da859716be5
Adam Orr <adamjorr@gmail.com>
2019-07-24 18:30:42 -0700

    fix benchmark not looking at all reads

--- tests/test_benchmark.py
+++ tests/test_benchmark.py
@@ -17,7 +17,7 @@ def test_benchmarks_match(monkeypatch, capfd):
     bam_captured = capfd.readouterr()
 
     with monkeypatch.context() as m:
-        m.setattr(sys, 'argv', [sys.argv[0]] + "benchmark -b tests/data/conf_regions.bam -r tests/data/ref.fa -v tests/data/conf_regions.vcf.gz --label=label -f tests/data/reads.fq".split())
+        m.setattr(sys, 'argv', [sys.argv[0]] + "benchmark -b tests/data/conf_regions.bam -r tests/data/ref.fa -v tests/data/conf_regions.vcf.gz --label=label -f tests/data/allreads.fq".split())
         kbbq.main.main()
     fastq_captured = capfd.readouterr()
     assert bam_captured.out == fastq_captured.out
