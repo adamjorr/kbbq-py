9734d4b247792ebe3ab8f5e19829be1b263f2c05
Adam Orr <adamjorr@gmail.com>
2019-08-06 11:12:50 -0700

    add plotting tests

--- tests/test_plot.py
+++ tests/test_plot.py
@@ -13,3 +13,19 @@ def test_benchmark_plot(monkeypatch, benchmarkfile, tmp_path):
         m.setattr(sys, 'argv', [sys.argv[0],'plot', '-o', str(outfile), benchmarkfile])
         kbbq.main.main()
     assert outfile.stat().st_size > 0
+
+def test_benchmark_plot_samplesize(monkeypatch, benchmarkfile, tmp_path):
+    """
+    Testing that the output file is actually written to is probably
+    the best we can do. No telling if the plot actually makes sense...
+    """
+    outfile = tmp_path / 'plot-sample-size.pdf'
+    with monkeypatch.context() as m:
+        m.setattr(sys, 'argv', [sys.argv[0],'plot', '-t', 'sample-size',
+            '-o', str(outfile), benchmarkfile])
+        kbbq.main.main()
+    assert outfile.stat().st_size > 0
+
+def test_plot_benchmark_invalid_type():
+    with pytest.raises(ValueError):
+        kbbq.plot.plot_benchmark(None, None, 'foo')
