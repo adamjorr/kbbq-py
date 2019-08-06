b1d20f070db4cbb9f4971c5708ec5861f490e7ea
Adam Orr <adamjorr@gmail.com>
2019-08-06 16:38:00 -0700

    add tests for recalibrate

--- tests/test_recalibrate.py
+++ tests/test_recalibrate.py
@@ -1,25 +1,102 @@
 import pytest
 from test_compare_reads import FakeRead, bamread_to_fakeread
-from kbbq import compare_reads
+import kbbq.main
 from kbbq import recalibrate
-from kbbq import benchmark
-from kbbq.gatk import bqsr
+from kbbq import compare_reads
 import pysam
-import contextlib
 import numpy as np
 
-def test_fastq_to_covariate_arrays(recalibratedbam, variable_sites, monkeypatch):
-    """
-    TODO
-    """
-    pass
+def test_find_corrected_sites(simple_fastq_reads):
+    for r in simple_fastq_reads:
+        r2 = pysam.FastxRecord(name = r.name, sequence = r.sequence, quality = r.quality)
+        edited_seq = list(r2.sequence)
+        edited_seq[5] = 'C'
+        r2.sequence = ''.join(edited_seq)
+        correct = np.zeros(len(edited_seq), dtype = np.bool)
+        correct[5] = True
+        assert np.array_equal(recalibrate.find_corrected_sites(r,r2), correct)
 
-def test_find_corrected_sites(simple_fastq, tmp_path):
-    """
-    TODO
-    """
-    corrected_fastq = tmp_path / 'corrected.fq'
-    with open(simple_fastq,'r') as fin, open(corrected_fastq,'w') as fout:
-        inlines = fin.readlines()
-        inlines[5] = inlines[5][0:5] + 'C' + inlines[5][6:]
-        fout.writelines(inlines)
+@pytest.fixture()
+def uncorr_and_corr_fastq_files(tmp_path):
+    uncorr_fastq = tmp_path / 'uncorr.fq'
+    corrected_fastq = tmp_path / 'corr.fq'
+    with open(uncorr_fastq,'w') as fu, open(corrected_fastq,'w') as fc:
+        r = pysam.FastxRecord(
+            name = 'foo',
+            sequence = 'ATG',
+            quality = '((#') #7, 7, 2
+        r2 = pysam.FastxRecord(
+            name = r.name,
+            sequence = 'ACG',
+            quality = r.quality)
+        fu.write(str(r))
+        fc.write(str(r2))
+    return str(uncorr_fastq), str(corrected_fastq)
+
+def test_fastq_to_covariate_arrays(uncorr_and_corr_fastq_files):
+    correct_pos_errs = np.zeros((1,43,6))
+    correct_pos_total = np.zeros((1,43,6))
+    correct_pos_errs[0,7,1] = 1
+    correct_pos_total[0,7,0] = 1
+    correct_pos_total[0,7,1] = 1
+    correct_dinuc_errs = np.zeros((1,43,16))
+    correct_dinuc_total = np.zeros((1,43,16))
+    correct_dinuc_errs[0, 7, compare_reads.Dinucleotide.dinuc_to_int['AT']] = 1
+    correct_dinuc_total[0, 7, compare_reads.Dinucleotide.dinuc_to_int['AT']] = 1
+    correct_vectors = [np.array([6]), #meanq
+        np.array([1]), #rg
+        np.array([2]), #rg
+        np.array([[0,0,0,0,0,0,0,1] + [0] * 35]), #q
+        np.array([[0,0,0,0,0,0,0,2] + [0] * 35]), #q
+        correct_pos_errs, #pos
+        correct_pos_total, #pos
+        correct_dinuc_errs, #dinuc
+        correct_dinuc_total] #diunc
+
+    for a,b in zip(correct_vectors, recalibrate.fastq_to_covariate_arrays(
+        uncorr_and_corr_fastq_files)):
+            assert np.array_equal(a,b)
+
+#this read is used below
+correct_read = pysam.FastxRecord(
+        name = 'foo',
+        sequence = 'ATG',
+        quality = '\'\'#') #6, 6, 2
+
+def test_recalibrate_fastq(uncorr_and_corr_fastq_files, capfd):
+    recalibrate.recalibrate_fastq(uncorr_and_corr_fastq_files)
+    captured = capfd.readouterr()
+    assert captured.out == str(correct_read) + '\n'
+
+    #TODO: we may want test 1000x this read to see a more realistic example
+
+def test_recalibrate_bam():
+    with pytest.raises(NotImplementedError):
+        recalibrate.recalibrate_bam(None)
+
+def test_recalibrate(uncorr_and_corr_fastq_files, capfd):
+    recalibrate.recalibrate(bam = None, fastq = uncorr_and_corr_fastq_files)
+    captured = capfd.readouterr()
+    assert captured.out == str(correct_read) + '\n'
+
+    with pytest.raises(NotImplementedError):
+        recalibrate.recalibrate(fastq = None, bam = 'foo')
+
+    with pytest.raises(NotImplementedError):
+        recalibrate.recalibrate(fastq = None, bam = None, gatkreport = 'foo')
+
+def test_recalibrate_main(uncorr_and_corr_fastq_files, monkeypatch, capfd):
+    import sys
+    with monkeypatch.context() as m:
+        m.setattr(sys, 'argv', [sys.argv[0]] + ["recalibrate",'-f'] + list(uncorr_and_corr_fastq_files) )
+        kbbq.main.main()
+    captured = capfd.readouterr()
+    assert captured.out == str(correct_read) + '\n'
+
+    with pytest.raises(NotImplementedError), monkeypatch.context() as m:
+        m.setattr(sys, 'argv', [sys.argv[0]] + ["recalibrate",'-b', 'foo'])
+        kbbq.main.main()
+
+    with pytest.raises(NotImplementedError), monkeypatch.context() as m:
+        m.setattr(sys, 'argv', [sys.argv[0]] + ["recalibrate",'-b', 'foo', '-g', 'bar'])
+        kbbq.main.main()
