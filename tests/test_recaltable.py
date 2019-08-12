2d88036d89faa2069f663b7d6108362280da43dc
Adam Orr <adamjorr@gmail.com>
2019-08-12 10:04:20 -0700

    add a few more tests

--- tests/test_recaltable.py
+++ tests/test_recaltable.py
@@ -1,6 +1,7 @@
 import pytest
 from kbbq import recaltable
 import filecmp
+import numpy as np
 
 ###########
 
@@ -52,9 +53,63 @@ def test_GATKReport_repr():
 
 def test_GATKReport_eq():
     report = recaltable.GATKReport([0,1,2])
-    other = recaltable.GATKReport([0,1,2])
-    assert report == other
-
+    assert report == recaltable.GATKReport([0,1,2])
+    assert not report == recaltable.GATKReport([4,5,6])
     assert not report == recaltable.GATKReport([])
-
+    assert not report == recaltable.GATKReport([], version = '0.0')
     assert not report == 3
+
+######################
+
+# GATKTable
+
+######################
+
+def test_GATKTable_init():
+    table = recaltable.GATKTable(title = 'foo', description = 'bar', data = 'baz')
+    assert table.title == 'foo'
+    assert table.description == 'bar'
+    assert table.data == 'baz'
+
+@pytest.fixture
+def extablestr():
+    return '''#:GATKTable:6:2:%s:%s:%.4f:%.4f:%d:%.2f:;
+#:GATKTable:RecalTable0:
+ReadGroup                   EventType  EmpiricalQuality  EstimatedQReported  Observations  Errors 
+HJCMTCCXX160113.5.AAGGATGT  M                   22.0000             24.3199        210398  1382.00
+HK2WYCCXX160124.1.AAGGATGT  M                   22.0000             24.3994        196298  1391.00
+'''
+
+def test_GATKTable_fromstring(extablestr):
+    table = recaltable.GATKTable.fromstring(extablestr)
+    assert table.title == 'RecalTable0'
+    assert table.description == ''
+    assert table.data.shape == (2, 6)
+
+@pytest.fixture
+def extable(extablestr):
+    return recaltable.GATKTable.fromstring(extablestr)
+
+def test_GATKTable_parse_fmtstring():
+    typedict = recaltable.GATKTable.parse_fmtstring(
+        header = ['foo','bar','baz','test'],
+        fmtstring = '#:GATKTable:0:4:%d:%.4f:%s:%x:;')
+    assert typedict == {
+        'foo': np.int64,
+        'bar': np.float64,
+        'baz': str }
+
+#def test_GATKTable_get_fmtstring(extable):
+#    print(extable)
+#    assert extable.get_fmtstring() == '#:GATKTable:6:2:%s:%s:%.4f:%.4f:%d:%.2f:;'
+
+#def test_GATKTable_get_colfmts(extable):
+#    assert extable.get_colfmts() == ['%s','%s','%.4f','%.4f','%d','%.2f']
+
+#def test_GATKTable_get_titlestring():
+#    table = recaltable.GATKTable(title = 'foo', description = 'bar', data = '')
+#    assert table.get_titlestring() == '#:GATKTable:foo:bar'
+
+#def test_GATKTable_get_datastring(extable, extablestr):
+#    assert extable.get_datastring() == '\n'.join(extablestr.splitlines()[2:])
+
