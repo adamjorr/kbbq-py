6b674fe34c2387f17c6a4c8e15ae9e2342995309
Adam Orr <adamjorr@gmail.com>
2019-10-11 16:39:22 -0700

    update __all__

--- kbbq/__init__.py
+++ kbbq/__init__.py
@@ -5,5 +5,6 @@ kbbq
 A package for reference-free base quality score recalibration.
 """
 
-__all__ = ['compare_reads', 'recaltable', 'plot', 'benchmark']
+__all__ = ['gatk','benchmark','bloom','compare_reads','covariate','plot','read',
+    'recalibrate','recaltable']
 __version__ = '0.0.0'
