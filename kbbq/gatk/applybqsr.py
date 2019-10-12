d02287e95582453fa53af241146651b414b1414b
Adam Orr <adamjorr@gmail.com>
2019-10-11 17:39:29 -0700

    add more documentation

--- kbbq/gatk/applybqsr.py
+++ kbbq/gatk/applybqsr.py
@@ -13,8 +13,26 @@ from .. import recaltable
 import collections
 
 def table_to_vectors(table, rg_order, maxscore = 42):
-    #the recal table uses the PU of the read group as the read group entry in the table
-    #see vectors_to_report for more info
+    """
+    Get a tuple of recalibration data vectors from a :class:`RecalibrationReport`.
+
+    The recal table uses the PU of the read group as the read group entry in the table.
+    See :func:`vectors_to_report` for more info
+
+    The rg_order must be given to define the output order of read groups, since there
+    is no natural ordering for them. If the PUs listed in the table are 'apple' and 
+    'orange', and rg_order is ['apple', 'orange'], data from the 'apple' read group
+    will be in index 0 and data from the 'orange' read group will be in index 1.
+
+    :param table: Report to get data from
+    :type table: :class:`RecalibrationReport`
+    :param rg_order: List of PU's controlling the order of read groups.
+    :type rg_order: list(str)
+    :param maxscore: largest quality score possible
+    :type maxscore: int
+    :return: data vectors (meanq, global_errs, global_total, q_errs, q_total, pos_errs, pos_total, dinuc_errs, dinuc_total)
+    :rtype: tuple of :class:`numpy.ndarray` of int
+    """
     dinuc_order = utils.Dinucleotide.dinuc_to_int.keys()
     rgtable = table.tables[2].data.reindex(rg_order)
     meanq = rgtable['EstimatedQReported'].values.astype(np.float64)
@@ -45,12 +63,33 @@ def table_to_vectors(table, rg_order, maxscore = 42):
     return meanq, global_errs, global_total, q_errs, q_total, pos_errs, pos_total, dinuc_errs, dinuc_total
 
 def bamread_cycle_covariates(read):
+    """
+    Get the cycle covariate from a bam read.
+
+    :param read: read to get covariate values from
+    :type read: :class:`pysam.AlignedSegment`
+    :return: cycle covariate
+    :rtype: :class:`numpy.ndarray` of int
+    """
     cycle = utils.generic_cycle_covariate(read.query_length, read.is_read2)
     if read.is_reverse:
         cycle = np.flip(cycle)
     return cycle
 
 def bamread_dinuc_covariates(read, use_oq = True, minscore = 6):
+    """
+    Get the dinuc covariates from a bam read.
+
+    These range from 0 to 15 for each possible dinucleotide, or -1 for an invalid
+    value. The first base is always invalid (since there is no base before it) and
+    any base overlapping an N will be invalid. Any base with a quality score below
+    minscore is also invalid.
+
+    :param read: read to get covariate values from
+    :type read: :class:`pysam.AlignedSegment`
+    :return: dinuc covariate
+    :rtype: :class:`numpy.ndarray` of int
+    """
     seq = read.query_sequence
     quals = (utils.bamread_get_oq(read) if use_oq else np.array(read.query_qualities, dtype = np.int))
     if read.is_reverse:
@@ -64,6 +103,20 @@ def bamread_dinuc_covariates(read, use_oq = True, minscore = 6):
     return dinuccov
 
 def recalibrate_bamread(read, meanq, globaldeltaq, qscoredeltaq, positiondeltaq, dinucdeltaq, rg_to_int, use_oq = True, minscore = 6):
+    """
+    Recalibrate a bamread and return the new quality scores.
+
+    Note that this function doesn't alter the given read.
+
+    :param read: read to recalibrate
+    :type read: :class:`pysam.AlignedSegment`
+    :param others: delta q values where the indices represent the rg, q, and dinuc or cycle covariates.
+    :type others: :class:`numpy.ndarray` of int
+    :param rg_to_int: dictionary to convert rg IDs to ints
+    :type rg_to_int: dict(str, int)
+    :return: recalibrated quality scores
+    :rtype: :class:`numpy.ndarray` of int
+    """
     complement = utils.Dinucleotide.complement
     
     original_quals = (utils.bamread_get_oq(read) if use_oq else np.array(read.query_qualities, dtype = np.int))
@@ -79,10 +132,21 @@ def recalibrate_bamread(read, meanq, globaldeltaq, qscoredeltaq, positiondeltaq,
     return recalibrated_quals
 
 def get_delta_qs(meanq, rg_errs, rg_total, q_errs, q_total, pos_errs, pos_total, dinuc_errs, dinuc_total):
-    # shapes are:
-    #   [rg]
-    #   [rg, q]
-    #   [rg, q, covariate]
+    """
+    Find the covariate delta Q's as arrays given the data arrays.
+
+    The input vector shapes are: [rg], [rg, q], [rg, q, covariate]. Returns a tuple
+    of delta Q arrays. The indices specify the covariate values, the values specify
+    how much the Q score should change based on its covariate values.
+
+    The preferred method of getting the covariate delta Q's is the 
+    :func:`get_modeldqs_from_covariates` function.
+
+    :param all: data array
+    :type all: :class:`numpy.ndarray` of int
+    :return: Delta Q arrays (rgdeltaq, qscoredeltaq, positiondeltaq, dinucdeltaq)
+    :rtype: tuple of :class:`numpy.ndarray` of int
+    """
     #seqlen = pos_total.shape[2] / 2
     nrgs = meanq.shape[0]
     rgdeltaq = utils.gatk_delta_q(meanq, rg_errs, rg_total)
@@ -107,6 +171,14 @@ ModelDQs = collections.namedtuple('ModelDQs', ['mean','rg','q','cycle','dinuc'],
     module = __name__)
 
 def get_modeldqs_from_covariates(covariates):
+    """
+    Find the covariate delta Q's given covariate data.
+
+    :param covariates: covariate data
+    :type covariates: :class:`CovariateData`
+    :returns: delta Q's
+    :rtype: :class:`.ModelDQs`
+    """
     nrgs = covariates.qcov.rgcov.num_rgs()
     expected_errs = np.sum(utils.q_to_p(np.arange(covariates.qcov.num_qs()))[np.newaxis,:] * covariates.qcov.total, axis = 1)
     meanq = utils.p_to_q(expected_errs / covariates.qcov.rgcov.total)
