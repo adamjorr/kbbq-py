73b9620e726ec46301b93147cef0c67a6904548d
Adam Orr <adamjorr@gmail.com>
2020-03-03 08:55:20 -0700

    bump to version 0.0.1

--- kbbq/__init__.py
+++ kbbq/__init__.py
@@ -4,4 +4,4 @@ A package for reference-free base quality score recalibration.
 
 __all__ = ['gatk','benchmark','bloom','compare_reads','covariate','plot','read',
     'recalibrate','recaltable']
-__version__ = '0.0.0'
+__version__ = '0.0.1'
